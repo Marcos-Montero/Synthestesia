f9df45d11275ed1db0bbe8d09e7b8775d17fe3a5
Marcos M. Rod <66304414+MarcosMRod@users.noreply.github.com>
2023-08-12 02:16:08 +0200

    add MelmothGoTo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useAppContext } from "./context/Context"
 import { CircleButton } from "./components/CircleButton"
 import { keys } from "./constants/keys"
+import { MelmothGoTo } from "./components/MelmothGoTo"
 
 const App = () => {
 	const { keyPressed } = useAppContext()
@@ -17,6 +18,7 @@ const App = () => {
 			}}
 			className="flex flex-col min-h-screen min-w-screen items-center justify-center"
 		>
+			<MelmothGoTo />
 			<div className="flex flex-wrap max-w-sm justify-center bg-slate-900/05 p-9 gap-4 rounded-3xl backdrop-blur-sm shadow-xl">
 				{keys.map(({ note, color, key }) => (
 					<CircleButton key={key} note={note} color={color} pressed={keyPressed?.key === key} />
---
+++ src/components/MelmothGoTo.tsx
@@ -0,0 +1,16 @@
+import { TiHome } from "react-icons/ti"
+export const MelmothGoTo = () => {
+	return (
+		<>
+			<a
+				href="https://melmoth.dev"
+				className="group/melmoth fixed p-2 rounded-lg shadow-xl shadow-black/50 top-8 left-8 bg-yellow-700/70 hover:bg-yellow-700/80 hover:text-white cursor-pointer w-12 duration-300 hover:w-[148px] overflow-hidden flex items-center h-16 justify-between"
+			>
+				<p>
+					<TiHome className=" text-white text-3xl" />
+				</p>
+				<span className="text-white opacity-0 group-hover/melmoth:opacity-100 text-md text-white/200 ease-in duration-300">Melmoth.dev</span>
+			</a>
+		</>
+	)
+}
